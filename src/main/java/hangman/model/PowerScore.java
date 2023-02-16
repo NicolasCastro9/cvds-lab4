f086ec65e6fc50ed5f46790707ab741fc1ffd88c
NicolasCastro9 <tankiandown@gmail.com>
2023-02-16 17:57:00 -0500

    PowerScore

--- src/main/java/hangman/model/PowerScore.java
+++ src/main/java/hangman/model/PowerScore.java
@@ -2,10 +2,34 @@ package hangman.model;
 
 public class PowerScore implements GameScore {
 
-    public int calculateScore(int correctCount, int incorrectCount)throws ModelException{
-        if(correctCount < 0 || incorrectCount < 0)throw new ModelException(ModelException.NUMERO_NEGATIVO)
-        return incorrectCount;
 
+    /**
+    @pre Se inicia el juego con 0 puntos
+	@pos Por cada letra incorrecta se penalizaran 8 puntos, la i-esima letra correcta se bonificara con 5^i
+    @pos Si con las reglas anteriores sobrepasa 500 puntos, el puntaje es 500.
+	@param int correctCount respuestas correctas
+	@param int incorrectCount respuestaas incorrectas
+	@throws ModelException.Numero_Negativo cuando el puntaje sea menor a 0
+	@return puntaje **/
+
+    @Override
+    public int calculateScore(int correctCount, int incorrectCount)throws ModelException{
+        if(correctCount < 0 || incorrectCount < 0)throw new ModelException(ModelException.NUMERO_NEGATIVO);
+        else{
+            int puntaje = 0;
+            for(int i=0; i<correctCount;i++){
+                puntaje += Math.pow(5,i+1);
+            }
+            puntaje -= incorrectCount * 8;
+            if(puntaje < 0){
+                return 0;
+            }
+            else if(puntaje > 500){
+                return 500;
+            }
+            else{
+                return puntaje;
+            }
+        }
     }
-    
 }
