bddb30da78418f8ecbacf493549482bc06811a22
NicolasCastro9 <tankiandown@gmail.com>
2023-02-16 17:33:20 -0500

    exceptions

--- src/main/java/hangman/model/PowerScore.java
+++ src/main/java/hangman/model/PowerScore.java
@@ -2,7 +2,8 @@ package hangman.model;
 
 public class PowerScore implements GameScore {
 
-    public int calculateScore(int correctCount, int incorrectCount){
+    public int calculateScore(int correctCount, int incorrectCount)throws ModelException{
+        if(correctCount < 0 || incorrectCount < 0)throw new ModelException(ModelException.NUMERO_NEGATIVO)
         return incorrectCount;
 
     }
