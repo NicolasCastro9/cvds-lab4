08ce1e64330c363aff8503faeea9ea8455f6941f
sergio-g-v <sergio.gonzalez-v@mail.escuelaing.edu.co>
2023-02-16 17:35:42 -0500

    OriginalScore update

--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -1,9 +1,12 @@
 package hangman.model;
 
-public class OriginalScore implements GameScore{
+public class OriginalScore implements GameScore {
+    public OriginalScore(){}
 
-    public int calculateScore(int correctCount, int incorrectCount){
-
-        return 0;
+    public int calculateScore(int correctCount, int incorrectCount) throws ModelException {
+        if(correctCount < 0 || incorrectCount < 0){
+            throw new ModelException(" Menor que 0 ");
+        }
+        return 100 - (incorrectCount * 10);
     }
 }
