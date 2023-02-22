8997d9d1b5513d0be2bde4178f9f8f426f298bee
NicolasCastro9 <tankiandown@gmail.com>
2023-02-22 10:46:47 -0500

    factory

--- src/main/java/hangman/setup/factoryMethod/HangmanDefaultFactoryMethod.java
+++ src/main/java/hangman/setup/factoryMethod/HangmanDefaultFactoryMethod.java
@@ -1,7 +1,7 @@
 package hangman.setup.factoryMethod;
-
 import hangman.model.English;
 import hangman.model.Language;
+import hangman.model.OriginalScore;
 import hangman.model.dictionary.EnglishDictionaryDataSource;
 import hangman.model.dictionary.HangmanDictionary;
 import hangman.view.HangmanPanel;
@@ -22,4 +22,9 @@ public class HangmanDefaultFactoryMethod extends HangmanFactoryMethod {
     public HangmanPanel createHangmanPanel() {
         return new HangmanStickmanPanel();
     }
+    @Override
+    public OriginalScore createOriginalScore() {
+        return new OriginalScore();
+    }
+
 }
